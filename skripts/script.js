f890ff8562246a1830fe322b52465e04ce5e6c65
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-07-08 02:30:25 +0300

    add target

--- skripts/script.js
+++ skripts/script.js
@@ -8,4 +8,25 @@ headerCityButton.addEventListener('click', () => {
   localStorage.setItem('lamode-location', city)
 });
 
+//Модалка
 
+const subheaderСart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cartModalOpen = () => {
+  cartOverlay.classList.add('cart-overlay-open');
+};
+
+const cartModalClose = () => {
+  cartOverlay.classList.remove('cart-overlay-open');
+}
+
+subheaderСart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+  const target = event.target;
+  if(target.matches('.cart__btn-close') 
+  || target.matches('.cart-overlay')) {
+    cartModalClose();
+  }
+})
