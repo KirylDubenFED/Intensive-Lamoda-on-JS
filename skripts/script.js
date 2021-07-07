dfb833f6ef9b7ef90665c8cc2699512aecd5618f
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-07-08 02:42:46 +0300

    disable scroll

--- skripts/script.js
+++ skripts/script.js
@@ -8,6 +8,19 @@ headerCityButton.addEventListener('click', () => {
   localStorage.setItem('lamode-location', city)
 });
 
+//блолкировка скролла
+
+const disableScroll = () => {
+  const widthScroll = window.innerWidth - document.body.offsetWidth;
+  document.body.style.cssText = `
+  overflow: hidden;
+  padding-right: ${widthScroll}px`
+}
+
+const enableScroll = () => {
+  document.body.style.cssText = '';
+}
+
 //Модалка
 
 const subheaderСart = document.querySelector('.subheader__cart');
@@ -15,18 +28,22 @@ const cartOverlay = document.querySelector('.cart-overlay');
 
 const cartModalOpen = () => {
   cartOverlay.classList.add('cart-overlay-open');
+  disableScroll();
 };
 
 const cartModalClose = () => {
   cartOverlay.classList.remove('cart-overlay-open');
+  enableScroll();
 }
 
 subheaderСart.addEventListener('click', cartModalOpen);
 
 cartOverlay.addEventListener('click', event => {
   const target = event.target;
-  if(target.matches('.cart__btn-close') 
-  || target.matches('.cart-overlay')) {
+  if (target.matches('.cart__btn-close')
+    || target.matches('.cart-overlay')) {
     cartModalClose();
   }
-})
+});
+
+
