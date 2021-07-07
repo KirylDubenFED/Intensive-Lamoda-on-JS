68011a83a946b33b8716131f54e13e6985a24e32
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-07-08 01:58:55 +0300

    fix script

--- skripts/script.js
+++ skripts/script.js
@@ -1,6 +1,13 @@
 const headerCityButton = document.querySelector('.header__city-button');
 
+if (localStorage.getItem('lamode-location')) {
+  headerCityButton.textContent = localStorage.getItem('lamode-location');
+}
+
 headerCityButton.addEventListener('click', () => {
-    const city = prompt('Уажите Ваш город')
-    headerCityButton.textContent = city
-})
+  const city = prompt('Уажите Ваш город')
+  headerCityButton.textContent = city
+  localStorage.setItem('lamode-location', city)
+});
+
+
